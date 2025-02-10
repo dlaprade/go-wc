6d3fe5f58ba0855f16f74c73809e89de9bbfeb67
Daniel LaPrade <dlaprade+bitbucket@gmail.com>
2025-02-10 09:54:07 -0500

    reformatting

--- wc_test.go
+++ wc_test.go
@@ -1,139 +1,139 @@
-package main 
+package main
 
 import (
-  "testing"
-  "os"
+	"os"
+	"testing"
 )
 
 func TestProcessLength(t *testing.T) {
-  file, err := os.Open("test.txt")
-  if err != nil {
-    
-  }
-  defer file.Close()
+	file, err := os.Open("test.txt")
+	if err != nil {
 
-  data := Data{}
+	}
+	defer file.Close()
 
-  t.Cleanup( resetFlags )
-  lengthFlag = true
+	data := Data{}
 
-  Process(file, &data)
+	t.Cleanup(resetFlags)
+	lengthFlag = true
 
-  if data.line != 79 {
-    t.Errorf("Expected line length: %d but received: %d", 79, data.line)
-  }
+	Process(file, &data)
+
+	if data.line != 79 {
+		t.Errorf("Expected line length: %d but received: %d", 79, data.line)
+	}
 }
 
 func TestProcessBytes(t *testing.T) {
-  file, err := os.Open("test.txt")
-  if err != nil {
-    
-  }
-  defer file.Close()
+	file, err := os.Open("test.txt")
+	if err != nil {
 
-  data := Data{}
+	}
+	defer file.Close()
 
-  bytesFlag = true
+	data := Data{}
 
-  t.Cleanup( resetFlags )
-  Process(file, &data)
+	bytesFlag = true
 
-  if data.bytes != 342190 {
-    t.Errorf("Expected bytes: %d but received: %d", 342190, data.bytes)
-  }
+	t.Cleanup(resetFlags)
+	Process(file, &data)
+
+	if data.bytes != 342190 {
+		t.Errorf("Expected bytes: %d but received: %d", 342190, data.bytes)
+	}
 }
 
 func TestProcessLines(t *testing.T) {
-  file, err := os.Open("test.txt")
-  if err != nil {
-    
-  }
-  defer file.Close()
+	file, err := os.Open("test.txt")
+	if err != nil {
 
-  data := Data{}
+	}
+	defer file.Close()
 
-  t.Cleanup( resetFlags )
-  linesFlag = true
+	data := Data{}
 
-  Process(file, &data)
+	t.Cleanup(resetFlags)
+	linesFlag = true
 
-  if data.lines != 7145 {
-    t.Errorf("Expected lines: %d but received: %d", 7145, data.lines)
-  }
+	Process(file, &data)
+
+	if data.lines != 7145 {
+		t.Errorf("Expected lines: %d but received: %d", 7145, data.lines)
+	}
 }
 
 func TestProcessCharacters(t *testing.T) {
-  file, err := os.Open("test.txt")
-  if err != nil {
-    
-  }
-  defer file.Close()
+	file, err := os.Open("test.txt")
+	if err != nil {
 
-  data := Data{}
+	}
+	defer file.Close()
 
-  t.Cleanup( resetFlags )
-  multibyteFlag = true
+	data := Data{}
 
-  Process(file, &data)
+	t.Cleanup(resetFlags)
+	multibyteFlag = true
 
-  if data.characters != 339292 {
-    t.Errorf("Expected characters: %d but received: %d", 339292, data.characters) 
-  }
+	Process(file, &data)
+
+	if data.characters != 339292 {
+		t.Errorf("Expected characters: %d but received: %d", 339292, data.characters)
+	}
 }
 
 func TestProcessWords(t *testing.T) {
-  file, err := os.Open("test.txt")
-  if err != nil {
-    
-  }
-  defer file.Close()
+	file, err := os.Open("test.txt")
+	if err != nil {
 
-  data := Data{}
+	}
+	defer file.Close()
 
-  t.Cleanup( resetFlags )
-  wordFlag = true
+	data := Data{}
 
-  Process(file, &data)
+	t.Cleanup(resetFlags)
+	wordFlag = true
 
-  if data.words != 58164 {
-    t.Errorf("Expected words: %d but received: %d", 58164, data.words)
-  }
+	Process(file, &data)
+
+	if data.words != 58164 {
+		t.Errorf("Expected words: %d but received: %d", 58164, data.words)
+	}
 }
 
 func TestProcessFailure(t *testing.T) {
-  file, err := os.Open("test.txt")
-  if err != nil {
-    
-  }
-  defer file.Close()
+	file, err := os.Open("test.txt")
+	if err != nil {
 
-  data := Data{}
+	}
+	defer file.Close()
 
-  t.Cleanup( resetFlags )
+	data := Data{}
 
-  Process(file, &data)
+	t.Cleanup(resetFlags)
 
-  if data.lines != 0 {
-    t.Errorf("Expected lines: %d but received: %d", 0, data.lines)
-  }
-  if data.bytes != 0 {
-    t.Errorf("Expected bytes: %d but received: %d", 0, data.bytes)
-  }
-  if data.characters != 0 {
-    t.Errorf("Expected characters: %d but received: %d", 0, data.characters)
-  }
-  if data.line != 0 {
-    t.Errorf("Expected line: %d but received: %d", 0, data.line)
-  }
-  if data.words != 0 {
-    t.Errorf("Expected words: %d but received: %d", 0, data.words)
-  }
+	Process(file, &data)
+
+	if data.lines != 0 {
+		t.Errorf("Expected lines: %d but received: %d", 0, data.lines)
+	}
+	if data.bytes != 0 {
+		t.Errorf("Expected bytes: %d but received: %d", 0, data.bytes)
+	}
+	if data.characters != 0 {
+		t.Errorf("Expected characters: %d but received: %d", 0, data.characters)
+	}
+	if data.line != 0 {
+		t.Errorf("Expected line: %d but received: %d", 0, data.line)
+	}
+	if data.words != 0 {
+		t.Errorf("Expected words: %d but received: %d", 0, data.words)
+	}
 }
 
 func resetFlags() {
-  lengthFlag    = false
-  bytesFlag     = false
-  multibyteFlag = false
-  linesFlag     = false
-  wordFlag      = false
+	lengthFlag = false
+	bytesFlag = false
+	multibyteFlag = false
+	linesFlag = false
+	wordFlag = false
 }
